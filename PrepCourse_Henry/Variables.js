e7a9d3c4d376f70e7841a465ec31ab6be53d26d8
B9inzon <tatopinzon369@gmail.com>
2024-06-15 15:04:31 -0500

    Data types

---
+++ PrepCourse_Henry/Variables.js
@@ -0,0 +1,26 @@
+//cadena de texto ()
+
+var cajon1 = 'bufanda verde';
+
+//numeros
+
+var numeroEntero = '27';
+var numeronegativo = '-3';
+var numeroDecimal = '4.58';
+
+//Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinNada = null;
+
+
