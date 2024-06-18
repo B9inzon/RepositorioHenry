1f330a0fa2b65ce61656d8d1535cf7d1d6a19bf7
B9inzon <tatopinzon369@gmail.com>
2024-06-18 10:37:53 -0500

    js file

---
+++ PrepCourse_Henry/bucles.js
@@ -0,0 +1,23 @@
+var suma = 0;
+//suma = suma + 1;
+//suma = suma + 2;
+//suma = suma + 3;
+//suma = suma + 4;
+//console.log(suma);
+
+
+//for (var i = 0; i < 10; i++) {
+//    suma = suma + 1;
+//    console.log(suma);
+//}
+
+/*for (var i = 0; i < 5; i++) {
+    suma = suma + i;
+    console.log('variable de iteración: ', i);
+}
+console.log('Variable suma: ', suma)   */
+
+while (suma < 5) {
+    suma = suma + 1;
+    console.log(suma);
+}
