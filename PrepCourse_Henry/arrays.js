c5c127138d0f1796b0908f1b5a9cac7dd7ef1f33
B9inzon <tatopinzon369@gmail.com>
2024-06-19 09:41:42 -0500

    Js arrays fle

---
+++ PrepCourse_Henry/arrays.js
@@ -0,0 +1,12 @@
+// var listaDeCompras = [];
+// listaDeCompras[3] = 'Tomates';
+// listaDeCompras[1] = 'Lechuga';
+
+// console.log(listaDeCompras.length);
+
+// listaDeCompras[1];
+// var elementoDelArray = listaDeCompras[3];
+// console.log(elementoDelArray);
+
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+console.log(nombres);
